19de31fdfb5522c6fa7f5c8c2e72fe66b2eaa203
Matthew Pickering <matthewtpickering@gmail.com>
2015-05-13 23:12:30 +0100

    Add example file and script to generate examples

---
+++ mkexamples.sh
@@ -0,0 +1,5 @@
+echo "" | pandoc --filter=example.hs -o examples/api.md
+echo "" | pandoc --filter=example.hs -o examples/api.docbook
+echo "" | pandoc --filter=example.hs -o examples/api.html
+echo "" | pandoc --filter=example.hs -o examples/api.haddock
+echo "" | pandoc --filter=example.hs -o examples/api.tex
